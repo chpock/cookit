e30212345c63422b7fb556b594e82d3c5987347f
Konstantin Kushnir <chpock@gmail.com>
2024-09-14 00:28:04 +0000

    Add x86_64-apple-darwin10.6 platform

--- build.sh
+++ build.sh
@@ -108,7 +108,7 @@ if [ "$1" != "build" ] && [ "$1" != "build-local" ]; then
     done
 
     if [ -z "$BUILD_PLATFORMS" ]; then
-        BUILD_PLATFORMS="i686-w64-mingw32 x86_64-w64-mingw32 i386-apple-darwin10.6 i686-pc-linux-gnu x86_64-pc-linux-gnu"
+        BUILD_PLATFORMS="i686-w64-mingw32 x86_64-w64-mingw32 i386-apple-darwin10.6 x86_64-apple-darwin10.6 i686-pc-linux-gnu x86_64-pc-linux-gnu"
         BUILD_ALL="$BUILD_HOME/all"
         rm -rf "$BUILD_ALL" "$BUILD_HOME/cookit"-*
     fi
@@ -414,6 +414,7 @@ vagrant_unlocked() {
 
 VAGRANT_VM="$(cat <<EOF
 i386-apple-darwin10.6 /c/DriveD/VM/loc-70-mac-101206-64
+x86_64-apple-darwin10.6 /c/DriveD/VM/loc-70-mac-101206-64
 i686-pc-linux-gnu /c/DriveD/VM/loc-26-lin-cent6-64
 x86_64-pc-linux-gnu /c/DriveD/VM/loc-26-lin-cent6-64
 EOF
