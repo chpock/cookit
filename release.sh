d9ee7da3302d2a448e94cbe388fa2f5b518ba8fe
Konstantin Kushnir <chpock@gmail.com>
2024-09-22 18:29:32 +0000

    Add more release automations

--- release.sh
+++ release.sh
@@ -5,6 +5,7 @@ set -e
 SELF_HOME="$(cd "$(dirname "$0")"; pwd)"
 BUILD_8_DIR="$SELF_HOME/build8"
 BUILD_9_DIR="$SELF_HOME/build9"
+BUILD_DIR="$SELF_HOME/build"
 RELEASE_DIR="$SELF_HOME/release"
 
 error() {
@@ -12,12 +13,17 @@ error() {
     exit 1
 }
 
-[ -d "$BUILD_8_DIR" ] || error "Build results for Tcl8 don't exist: $BUILD_8_DIR"
-[ -d "$BUILD_9_DIR" ] || error "Build results for Tcl9 don't exist: $BUILD_9_DIR"
+rm -rf "$BUILD_DIR" "$BUILD_8_DIR" "$BUILD_9_DIR" "$RELEASE_DIR"
+
+cd "$SELF_HOME"
+
+./build.sh --tcl-version 8 --buildtype release
+mv "$BUILD_DIR" "$BUILD_8_DIR"
+
+./build.sh --tcl-version 9 --buildtype release
+mv "$BUILD_DIR" "$BUILD_9_DIR"
 
 VERSION="$(cat "$BUILD_9_DIR"/x86_64-pc-linux-gnu/kit/x86_64-pc-linux-gnu/version)"
-
-rm -rf "$RELEASE_DIR"
 
 for PLATFORM in i686-w64-mingw32 x86_64-w64-mingw32 x86_64-apple-darwin10.6 i686-pc-linux-gnu x86_64-pc-linux-gnu; do
     mkdir -p "$RELEASE_DIR/cookit-$VERSION"
